f8c3d22421d67c2e868867f207a1253b27eb4a4d
Hamish Coleman <hamish@zot.org>
2017-09-13 20:02:40 +0800

    Extend test coverage

--- test_egv.py
+++ test_egv.py
@@ -2,12 +2,6 @@
 """ Perform unit tests on the egv.py file
 """
 
-#
-# This file has a syntax error in OneWireCRC - the return tries to return
-# an undefined variable.
-#
-# Thus - assume this file is unused for the moment and do not test anything
-#
 
 import unittest
 
@@ -15,14 +9,35 @@ import egv
 
 class TestegvClass(unittest.TestCase):
     def setUp(self):
-        self.object = egv.egv()
+        self.data = []
+        self.object = egv.egv(target=lambda s:self.data.append(s))
 
     def tearDown(self):
         self.object = None
 
+#
+# This file has a syntax error in OneWireCRC - the return tries to return
+# an undefined variable.
+#
+# Thus - assume that function is unused for the moment
+#
 #    def test_OneWireCRC(self):
 #        self.assertEqual(
 #            self.object.OneWireCRC([fixme]),
 #            fixme
 #        )
 
+    def test_make_move_data(self):
+
+        tests = [
+            { 'x': 0, 'y': 0, 'expect': '' },
+            { 'x': 1, 'y': 1, 'expect': 'ILaBaS1P' },
+            { 'x': 1000, 'y': 1000, 'expect': 'ILzzz235Bzzz235S1P' },
+        ]
+        # TODO - add more tests to demonstrate the edge cases
+
+        for test in tests:
+            self.data = []
+            self.object.make_move_data( test['x'], test['y'] )
+            got = "".join(map(chr, self.data))
+            self.assertEqual( got, test['expect'] )
