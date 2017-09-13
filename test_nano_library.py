e1265f178423a4bb4b2b4ebfe3aaafdae3308887
Hamish Coleman <hamish@zot.org>
2017-09-13 18:59:58 +0800

    Extend test coverage

--- test_nano_library.py
+++ test_nano_library.py
@@ -6,9 +6,36 @@ import unittest
 
 import nano_library
 
+class Fake_dev:
+    def __init__(self):
+        self.expect = None
+
+    def _handle(self, name):
+        if self.expect != name:
+            raise ValueError
+        self.expect = None
+
+    def reset(self):
+        self._handle('reset')
+
+    def write(self, addr, data, timeout):
+        self._handle('write')
+        self.write_addr = addr
+        self.write_data = data
+        self.write_timeout = timeout
+
+    def read(self, addr, buflen, timeout):
+        # TODO - this often comes after a write, so we cannot expect both
+        #self._handle('write')
+        self.read_addr = addr
+        self.read_buflen = buflen
+        self.read_timeout = timeout
+        return "slartibartfast"
+
 class TestK40_CLASS(unittest.TestCase):
     def setUp(self):
         self.object = nano_library.K40_CLASS()
+        self.object.dev = Fake_dev()
 
     def tearDown(self):
         self.object = None
@@ -23,6 +50,28 @@ class TestK40_CLASS(unittest.TestCase):
         self.assertEqual(unlock, '\xa6\x00IS2PFFFFFFFFFFFFFFFFFFFFFFFFFF\xa6\x0f')
         self.assertEqual(home,   '\xa6\x00IPPFFFFFFFFFFFFFFFFFFFFFFFFFFF\xa6\xe4')
         self.assertEqual(estop,  '\xa6\x00IFFFFFFFFFFFFFFFFFFFFFFFFFFFFF\xa6\x82')
+
+    def test_say_hello(self):
+        self.object.dev.expect = 'write'
+        self.assertEqual(self.object.say_hello(), ['slartibartfast'])
+        self.assertEqual(self.object.dev.write_data, self.object.hello)
+
+    def test_send_array(self):
+        test_data = 'aabbccdd'
+        self.object.dev.expect = 'write'
+        self.object.send_array(test_data)
+        # TODO - why does send_array not return the read_data results?
+        self.assertEqual(self.object.dev.write_data, test_data)
+
+    # Several of these function calls have nested calls to dev functions,
+    # which my dumb dev mock cannot handle:
+    #   unlock_rail() calls say_hello()
+    #   e_stop() calls say_hello()
+    #   home_position() calls say_hello()
+
+    def test_reset_usb(self):
+        self.object.dev.expect = 'reset'
+        self.object.reset_usb()
 
     def test_OneWireCRC(self):
         line = map(ord, 'AK0FFFFFFFFFFFFFFFFFFFFFFFFFFF')
@@ -44,3 +93,23 @@ class TestK40_CLASS(unittest.TestCase):
             self.object.estop[-1] 
         )
 
+    def test_send_data(self):
+        data = map(ord, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+        self.object.dev.expect = 'write'
+        self.object.send_data(data, update_gui=None, stop_calc=None)
+
+        # FIXME - confirm the data actually being sent
+
+    def test_rapid_move(self):
+        self.object.dev.expect = 'write'
+        self.object.rapid_move(1000, 1000)
+
+        # Deeper probing of this data block is done in test_egv
+        data = map(ord, '\xa6\x00ILzzz235Bzzz235S1PFFFFFFFFFFFF\xa6\x8a')
+        self.assertEqual(self.object.dev.write_data, data)
+
+    def test_hex2dec(self):
+        input = ["40","e7"]
+        output = [64, 231]
+        self.assertEqual(self.object.hex2dec(input), output)
